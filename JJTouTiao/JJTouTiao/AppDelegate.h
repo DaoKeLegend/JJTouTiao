0b992cae392acdfdef8e070cc8d1fd5940a31ea9
DaokeLegend <lijunyuwork@163.com>
2017-10-31 23:46:44 +0800

    JJTouTiao

---
+++ JJTouTiao/JJTouTiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJTouTiao
+//
+//  Created by lucy on 2017/10/31.
+//  Copyright © 2017年 com.daoKeLegend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJTouTiao/JJTouTiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJTouTiao
+//
+//  Created by lucy on 2017/10/31.
+//  Copyright © 2017年 com.daoKeLegend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
